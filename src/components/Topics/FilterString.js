50bbb9840b7ca97cd321c7d075c94e8c6e7f6821
Windwaker02 <woodringg95@gmail.com>
2019-05-20 14:45:33 -0400

    finshed FitlerString

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react';
 
 export default class FilterString extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            unFilteredArray: ['Greg'
+            , 'Hello how are you'
+            , 'The cake is a lie'
+            , 'Old Meme'
+            , 'Dank Meme'],
+            filteredArray: [],
+            userInput: ''
+        }
+    }
+
+    handleFilterClick = () => {
+        let filteredArray = this.state.unFilteredArray.filter(item => {
+            return item.includes(this.state.userInput)
+        })
+        this.setState({
+            filteredArray
+        })
+    }
 
     render(){
         return(
-            <p>FilterString</p>
+            <div>
+                <h4 className="puzzleBox evenAndOddPB">
+                FilterString
+                    <span className="resultsBox">
+                        {JSON.stringify(this.state.unFilteredArray)}
+                    </span>
+                    <span className="resultsBox">
+                    {JSON.stringify(this.state.filteredArray)}
+                    </span>
+                </h4>
+                <input 
+                className="inputLine"
+                onChange = {(e) => this.setState({userInput: e.target.value})}
+                />
+                <button 
+                onClick={this.handleFilterClick}
+                className="confirmationButton"> 
+                </button>
+                
+            </div>
         )
     }
 }
